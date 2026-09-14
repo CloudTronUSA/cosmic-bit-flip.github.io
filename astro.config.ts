fbd7ffbdcb2a4254987190ea9507c2c12d345c56
Sebastian Dowell <KN6BDL@protonmail.com>
2026-09-14 02:21:01 -0700

    some optimization

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,6 @@ import rehypeKatex from 'rehype-katex'
 import rehypePrettyCode from 'rehype-pretty-code'
 import remarkEmoji from 'remark-emoji'
 import remarkMath from 'remark-math'
-import rehypeDocument from 'rehype-document'
 
 import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
@@ -80,15 +79,16 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'load',
+  },
+  experimental: {
+    clientPrerender: true,
+  },
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
-      [
-        rehypeDocument,
-        {
-          css: 'https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css',
-        },
-      ],
       [
         rehypeExternalLinks,
         {
